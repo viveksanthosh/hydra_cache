71137ef4c4c949a03ef681142146a53578554c69
Vivek <vivek.santhosh@in.tesco.com>
2017-09-17 18:09:21 +0530

    added type safty for the initial files

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-const CacheFactory = ({ uniqueKey }) => {
+const CacheFactory = ({ uniqueKey, refresh, }) => {
+    if (!uniqueKey) {
+        throw new Error('Unique key not defined in options')
+    }
+
+    if (typeof refresh! == "function") {
+        throw new Error('Refresh in options is not a function')
+    }
 
 }
